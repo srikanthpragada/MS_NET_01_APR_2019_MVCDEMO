75132c14d741cf511ea60ff18d384052dd0eda42
srikanthpragada <srikanthpragada@gmail.com>
2019-04-25 09:33:32 +0530

    25-APR

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            return View(); // returns  Index.cshtml 
+        }
+
+        // Hello/Welcome 
+        public string Welcome()
+        {
+            return "Welcome To ASP.NET MVC";
+        }
+    }
+}
